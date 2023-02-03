f1f54cd0fcdb771d8da7df74c1a7b204c313fc0f
sergeyblagodarnyy <sergey.blagodarnyy@faramax.com>
2023-02-03 15:42:02 +0100

    Fixed library paths

--- utc-m4.cmake
+++ utc-m4.cmake
@@ -1,7 +1,9 @@
 set(ARM_TOOLS_PATH           "/home/sergey/projects/developUTC/source/arm-tools")
+set(STM32_CUBE_MP1_PATH      "${ARM_TOOLS_PATH}/STM32CubeMP1")
+
 set(STM32_TOOLCHAIN_PATH     "${ARM_TOOLS_PATH}/gcc")
-set(CROSS_PREFIX             "${ARM_TOOLS_PATH}/gcc/bin/arm-none-eabi-")
-set(CMAKE_SYSROOT            "${ARM_TOOLS_PATH}/gcc/arm-none-eabi/")
+set(CROSS_PREFIX             "${STM32_TOOLCHAIN_PATH}/bin/arm-none-eabi-")
+set(CMAKE_SYSROOT            "${STM32_TOOLCHAIN_PATH}/arm-none-eabi/")
 
 set(CMAKE_C_CXX_COMMON_FLAGS  "-Wno-unused-variable -Wno-unused-parameter -Wno-psabi -Wno-deprecated-declarations")
 set(CMAKE_C_FLAGS             "${CMAKE_C_CXX_COMMON_FLAGS}") #${STM32_FLAGS}
@@ -13,8 +15,9 @@ set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_C_COMPILER         "${CROSS_PREFIX}gcc")
 set(CMAKE_CXX_COMPILER       "${CROSS_PREFIX}g++")
 
-set(STM32_CUBE_MP1_PATH      "${ARM_TOOLS_PATH}/STM32CubeMP1")
+
 set(STM32_CUBE_PATH          "${STM32_CUBE_MP1_PATH}")
 set(FREERTOS_PATH            "${STM32_CUBE_MP1_PATH}/Middlewares/Third_Party/FreeRTOS")
 
-include(${ARM_TOOLS_PATH}/stm32-cmake/cmake/stm32_gcc.cmake)
+set(STM32_CMAKE_PATH         "${CMAKE_CURRENT_LIST_DIR}/stm32-cmake")
+include(${STM32_CMAKE_PATH}/cmake/stm32_gcc.cmake)
